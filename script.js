b99131b2cfc7e4c000130a61798d2aa2a3afc1cc
Jayanti_Sharma <jayantisharma271010@gmail.com>
2023-12-20 12:20:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// document.addEventListener('DOMContentLoaded',function(){
+//     const navbarCollapse = document.querySelector('.navbar-collapse')
+//     if(navbarCollapse.classList.contains('show')){
+//         console.log('yes')
+//     }else{
+//         console.log('no')
+//     }
+// })
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     const navbarCollapse = document.querySelector('.navbar-collapse')
+//     const navbarToggler = document.querySelector('.navbar-toggler')
+
+//     navbarToggler.addEventListener('click', function(){
+//         if(!navbarCollapse.classList.contains('show')){
+//             document.body.style.opacity = 0.08
+//         }else{
+//             document.body.style.opacity = 1
+
+//         }
+//     })
+// })
